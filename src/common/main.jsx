b7541f1850de0be174f763454eabcf3519088be9
TomasAlcalde <talcalden@uc.cl>
2023-07-02 19:44:23 -0400

    ;

--- src/common/main.jsx
+++ src/common/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import Navbar from './Navbar'
-import Routing from './Routing'
-import AuthProvider from '../auth/AuthProvider'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Navbar from './Navbar';
+import Routing from './Routing';
+import AuthProvider from '../auth/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <AuthProvider>
